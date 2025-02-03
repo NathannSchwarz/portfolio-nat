8abc87069c8d6dd0b9b3b093fb6d52cef20a52cb
Nathan <nathan.schwarz02@edu.univ-fcomte.fr>
2025-02-03 14:40:40 +0100

    edit composant projet

--- src/components/ProjectComp.vue
+++ src/components/ProjectComp.vue
@@ -1,19 +1,26 @@
 <template>
   <router-link :to="`/${id}`" class="flex flex-col group" @click="scrollToTop">
-    <img
-      :src="imageSrc"
-      :alt="imageAlt"
-      class="w-full object-cover mb-2 transition-transform duration-500 ease-in-out group-hover:scale-105"
-    />
+    <div
+      class="relative w-full overflow-hidden transition-all duration-500 ease-in-out group-hover:rounded-2xl"
+    >
+      <img
+        :src="imageSrc"
+        :alt="imageAlt"
+        class="w-full object-cover mb-2 transition-transform duration-500 ease-in-out group-hover:scale-110"
+      />
+    </div>
+
     <h2 class="text-2xl font-lactos md:text-3xl">{{ title }}</h2>
-    <p class="text-sm font-unbounded font-light text-coloblue hover:text-colored transition-colors duration-500 md:text-base">
+    <p
+      class="text-sm font-unbounded font-light text-coloblue hover:text-colored transition-colors duration-500 md:text-base"
+    >
       {{ subtitle }}
     </p>
   </router-link>
 </template>
 
 <script setup lang="ts">
-import { defineProps } from 'vue';
+import { defineProps } from 'vue'
 
 defineProps<{
   id: number
