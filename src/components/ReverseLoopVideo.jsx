92ffa331c8909e08f8a15a896d3ac1e9186cb266
Nathan <nathan.schwarz02@edu.univ-fcomte.fr>
2025-09-09 22:25:42 +0200

    revenir en arriere pour la video

--- src/components/ReverseLoopVideo.jsx
+++ src/components/ReverseLoopVideo.jsx
@@ -11,7 +11,7 @@ function ReverseLoopVideo(props) {
       style={{ pointerEvents: 'none', userSelect: 'none' }}
       {...props}
     >
-      <source src="https://res.cloudinary.com/dy048q5gh/video/upload/f_auto,q_auto,w_auto/v1757448805/Fleur_lzlvm1.mp4" type="video/mp4" />
+      <source src="/video/Fleur.mp4" type="video/mp4" />
       Votre navigateur ne supporte pas la lecture de vidéos.
     </video>
   )
